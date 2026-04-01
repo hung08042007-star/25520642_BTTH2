b95d3bc5c0ea5da54f3a74f671ef60157ebc444c
hung08042007-star <hung08042007@gmail.com>
2026-04-01 15:27:50 +0700

    bai2

--- main2.cpp
+++ main2.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
-#include "NgayThangNam.h"
-#include "NgayThangNam.cpp"
-using namespace std;
+#include "PhanSo.h"
+#include "PhanSo.cpp"
 int main() {
-    NgayThangNam a;
-    cout << "Nhap ngay thang nam: " << endl;
+    PhanSo a, b;
+
+    cout << "Nhap phan so A:\n";
     a.Nhap();
-    cout << "\nNgay thang nam vua nhap la: " << endl;
+
+    cout << "Nhap phan so B:\n";
+    b.Nhap();
+
+    cout << "\nPhan so A: ";
+    a.RutGon();
     a.Xuat();
-    a.NgayThangNamTiepTheo();
-    cout << "\nNgay thang nam tiep theo la: " << endl;
-    a.Xuat();
+
+    cout << "\nPhan so B: ";
+    b.RutGon();
+    b.Xuat();
+
+    // Tính toán
+    PhanSo tong = a.Tong(b);
+    PhanSo hieu = a.Hieu(b);
+    PhanSo tich = a.Tich(b);
+    PhanSo thuong = a.Thuong(b);
+
+    cout << "\n\nTong: ";
+    tong.Xuat();
+
+    cout << "\nHieu: ";
+    hieu.Xuat();
+
+    cout << "\nTich: ";
+    tich.Xuat();
+
+    cout << "\nThuong: ";
+    thuong.Xuat();
+
+    // So sánh
+    int cmp = a.SoSanh(b);
+    if (cmp == 1)
+        cout << "\nA > B";
+    else if (cmp == 0)
+        cout << "\nA = B";
+    else
+        cout << "\nA < B";
+
     return 0;
 }
