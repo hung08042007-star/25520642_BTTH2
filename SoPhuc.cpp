08311e7a3f964f84fe506e3beb277307d2154b81
hung08042007-star <hung08042007@gmail.com>
2026-04-01 15:49:35 +0700

    bai 3

---
+++ SoPhuc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "SoPhuc.h"
+using namespace std;
+void SoPhuc :: Nhap() {
+    cout << "Nhap phan thuc: ";
+    cin >> iThuc;
+    cout << "Nhap phan ao: ";
+    cin >> iAo;
+}
+void SoPhuc :: Xuat() {
+    cout << iThuc;
+    if (iAo >= 0)
+        cout << " + " << iAo << "i";
+    else
+        cout << " - " << -iAo << "i";
+}
+SoPhuc SoPhuc :: Tong(SoPhuc sp) {
+    SoPhuc kq;
+    kq.iThuc = iThuc + sp.iThuc;
+    kq.iAo = iAo + sp.iAo;
+    return kq;
+}
+SoPhuc SoPhuc :: Hieu(SoPhuc sp) {
+    SoPhuc kq;
+    kq.iThuc = iThuc - sp.iThuc;
+    kq.iAo = iAo - sp.iAo;
+    return kq;
+}
+SoPhuc SoPhuc :: Tich(SoPhuc sp) {
+    SoPhuc kq;
+    kq.iThuc = iThuc * sp.iThuc - iAo * sp.iAo;
+    kq.iAo = iThuc * sp.iAo + iAo * sp.iThuc;
+    return kq;
+}
+SoPhuc SoPhuc :: Thuong(SoPhuc sp) {
+    SoPhuc kq;
+    int mau = sp.iThuc * sp.iThuc + sp.iAo * sp.iAo;
+    kq.iThuc = (iThuc * sp.iThuc + iAo * sp.iAo) / mau;
+    kq.iAo = (iAo * sp.iThuc - iThuc * sp.iAo) / mau;
+    return kq;
+}
