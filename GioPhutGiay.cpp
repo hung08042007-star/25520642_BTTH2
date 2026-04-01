318b6468eb9aa80baba5ab772bd11d3a2725167d
hung08042007-star <hung08042007@gmail.com>
2026-04-01 16:09:52 +0700

    bai 4

---
+++ GioPhutGiay.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "GioPhutGiay.h"
+using namespace std;
+void GioPhutGiay :: Nhap() {
+    do {
+        cout << "Nhap gio (0-23): " << endl;
+        cin >> iGio;
+    } while (iGio < 0 && iGio > 23);
+    do {
+        cout << "Nhap phut (0-59): " << endl;
+        cin >> iPhut;
+    } while (iPhut < 0 && iPhut > 59);
+    do {
+        cout << "Nhap giay (0-59): " << endl;
+        cin >> iGiay;
+    } while (iGiay < 0 && iGiay > 59);
+}
+void GioPhutGiay :: Xuat() {
+    cout << iGio << ":" << iPhut << ":" << iGiay;
+}
+void GioPhutGiay :: TinhCongThemMotGiay() {
+    iGiay++;
+    if (iGiay == 60) {
+        iGiay == 0;
+        iPhut++;
+        if (iPhut == 60) {
+            iPhut == 0;
+            iGio++
+            if (iGio == 24) {
+                iGio == 0;
+            }
+        }
+    }
+}
